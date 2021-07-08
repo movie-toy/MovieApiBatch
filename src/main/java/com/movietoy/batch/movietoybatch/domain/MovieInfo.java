30fae0be1083fb3e8a20803036b691971f8273bf
Sangin-C <sangin3435@naver.com>
2021-07-08 16:02:00 +0900

    소스 정리

--- src/main/java/com/movietoy/batch/movietoybatch/domain/MovieInfo.java
+++ src/main/java/com/movietoy/batch/movietoybatch/domain/MovieInfo.java
@@ -19,37 +19,51 @@ public class MovieInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
+    /*영화코드*/
     private String movieCd;
 
+    /*영화명(국문)*/
     private String movieNm;
 
+    /*영화명(영문)*/
     private String movieNmEn;
 
+    /*영화명(원문)*/
     private String movieNmOg;
 
+    /*제작연도*/
     private String prdtYear;
 
+    /*개봉연도*/
     private String openDt;
 
+    /*제작상태*/
     private String prdtStatNm;
 
+    /*영화유형*/
     private String typeNm;
 
+    /*제작국가*/
     @Column(columnDefinition = "json")
     private String nations;
 
+    /*장르 정보*/
     @Column(columnDefinition = "json")
     private String genres;
 
+    /*감독*/
     @Column(columnDefinition = "json")
     private String directors;
 
+    /*배우*/
     @Column(columnDefinition = "json")
     private String actors;
 
+    /*참여 영화사*/
     @Column(columnDefinition = "json")
     private String companys;
 
+    /*심의정보*/
     @Column(columnDefinition = "json")
     private String audits;
 
--- src/main/java/com/movietoy/batch/movietoybatch/service/MovieApiService.java
+++ src/main/java/com/movietoy/batch/movietoybatch/service/MovieApiService.java
@@ -32,42 +32,57 @@ public class MovieApiService {
     }
 
     public MovieInfo selectMovieInfo(String movieCd) {
-        //영화 리스트 batch상태 코드 값 Y로 변경 ( Y = 상세 데이터 넣음, N = 상세 데이터 안넣음 )
+        //영화 리스트 batch상태 코드 값 Y로 변경 ( Y = 상세 데이터 넣음, NULL = 상세 데이터 안넣음 )
         MovieList movieList = movieListRepository.findByMovieCd(movieCd);
         MovieInfo movieInfo = new MovieInfo();
         movieInfo = movieApi.movieInfo(movieCd);
         //API에서 호출할 수 있는 일일 트래픽을 넘어갔으면 key를 바꾼다.
-        if(movieInfo == null){
+        if(movieInfo == null) {
+            System.out.println("********************key out********************");
             //상인 키 -> 호진 키
-            if(movieApi.getKey().equals("f778bea14d8ca8349bc583598d1288e9")){
+            if (movieApi.getKey().equals("f778bea14d8ca8349bc583598d1288e9")) {
+                System.out.println("********************key 상인 -> 호진********************");
                 movieApi.setKey("aa70b0701d04d2ab017306b706c486d8");
                 movieInfo = movieApi.movieInfo(movieCd);
-                movieList.setBatchStatus("Y");
-                movieListRepository.save(movieList);
+            }
+        }
+
+        if(movieInfo == null) {
+            System.out.println("********************key out********************");
             //호진 키 -> 홍제 키
-            } else if (movieApi.getKey().equals("aa70b0701d04d2ab017306b706c486d8")) {
+            if (movieApi.getKey().equals("aa70b0701d04d2ab017306b706c486d8")) {
+                System.out.println("********************key 호진 -> 홍제********************");
                 movieApi.setKey("6b52eea1663b4710d8589939f0097554");
                 movieInfo = movieApi.movieInfo(movieCd);
-                movieList.setBatchStatus("Y");
-                movieListRepository.save(movieList);
+            }
+        }
+
+        if(movieInfo == null) {
+            System.out.println("********************key out********************");
             //홍제 키 -> 상인 키2
-            } else if (movieApi.getKey().equals("6b52eea1663b4710d8589939f0097554")) {
+            if (movieApi.getKey().equals("6b52eea1663b4710d8589939f0097554")) {
+                System.out.println("********************key 홍제 -> 상인2********************");
                 movieApi.setKey("ac4b5031aa97be6f4c74686ce6b4fbf5");
                 movieInfo = movieApi.movieInfo(movieCd);
-                movieList.setBatchStatus("Y");
-                movieListRepository.save(movieList);
+            }
+        }
+
+        if(movieInfo == null) {
+            System.out.println("********************key out********************");
             //상인 키2 -> 호진 키2
-            } else if (movieApi.getKey().equals("ac4b5031aa97be6f4c74686ce6b4fbf5")) {
+            if (movieApi.getKey().equals("ac4b5031aa97be6f4c74686ce6b4fbf5")) {
+                System.out.println("********************key 상인2 -> 호진2********************");
                 movieApi.setKey("e396c6dfac8b425d0590a2c4d9b822a7");
                 movieInfo = movieApi.movieInfo(movieCd);
-                movieList.setBatchStatus("Y");
-                movieListRepository.save(movieList);
             }
-            return movieInfo;
-        }else{
+        }
+
+        if(movieInfo != null){
+            System.out.println("===============key OK================");
             movieList.setBatchStatus("Y");
             movieListRepository.save(movieList);
-            return movieInfo;
         }
+
+        return movieInfo;
     }
 }
